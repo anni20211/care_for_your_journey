1e45dc2178590d99a47cee8b6b27455bb38aad7a
ankit kumar <ankitkumar04082001@outlook.com>
2023-08-26 02:44:07 +0530

    latest packeage update

--- client/src/Hooks/useFetch.js
+++ client/src/Hooks/useFetch.js
@@ -4,7 +4,6 @@ const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
